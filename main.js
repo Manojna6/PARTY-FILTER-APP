fb444477bde97c8c27d2cee3770e2957a319dbb6
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-07-23 15:19:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,28 @@ function setup()
 {
     canvas = createCanvas(400, 400);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPosed);
 }
 function draw() {
-    
+    image(video, 0,0,400,400)
 }
 function take_snapshot() {
     
+}
+function modelLoaded()
+{
+    console.log("Posenet is initialized!")
+}
+function gotPosed(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+        console.log("nose-x = "+results[0].pose.nose.x);
+        console.log("nose-y = "+results[0].pose.nose.y);
+    }
 }
